824ee796dc4ed71818daecabfdd2f92c9eece422
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-09-18 09:02:21 +0530

    Add config parameters for create_folds

--- 4_Structured_MNIST_Project/src/config.py
+++ 4_Structured_MNIST_Project/src/config.py
@@ -1,2 +1,4 @@
-TRAINING_FILE = '../input/train_folds.csv'
+INPUT_TRAIN_FILE = '../input/train.csv'
+FOLDS_OUTPUT_FILE = '../input/train_folds.csv'
+TRAINING_FILE = FOLDS_OUTPUT_FILE
 MODELS_OUTPUT = '../models/'
