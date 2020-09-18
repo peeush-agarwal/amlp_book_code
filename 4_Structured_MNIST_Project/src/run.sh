3fbbabfc1778fe2bbec7e7d835c7ef910b81721d
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-09-18 09:23:58 +0530

    Update shell file to pass model name from CLI

--- 4_Structured_MNIST_Project/src/run.sh
+++ 4_Structured_MNIST_Project/src/run.sh
@@ -1,5 +1,11 @@
-python3 train.py --fold 0
-python3 train.py --fold 1
-python3 train.py --fold 2
-python3 train.py --fold 3
-python3 train.py --fold 4
+#!/bin/bash
+
+# Example: $ sh run.sh dt_gini
+# change model name after run.sh to run for other model defined in model_dispatcher
+
+MODEL=$1
+python3 train.py --fold 0 --model $MODEL
+python3 train.py --fold 1 --model $MODEL
+python3 train.py --fold 2 --model $MODEL
+python3 train.py --fold 3 --model $MODEL
+python3 train.py --fold 4 --model $MODEL
