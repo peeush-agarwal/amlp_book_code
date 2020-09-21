4ef5af6741434f309d23f581155c0a05808d9325
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-09-21 23:16:14 +0530

    Update ohe_logres.py more comments

--- 5_Approaching_Categorical_Variables/src/ohe_logres.py
+++ 5_Approaching_Categorical_Variables/src/ohe_logres.py
@@ -1,64 +1,66 @@
 import pandas as pd
 
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import KFold
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
 
-import joblib
-import os
-import argparse
-
 import config
 
+import os
+import joblib
+import argparse
+
 def run(fold):
-    # Load data into memory
+    # Load the data file into memory
     train_data = pd.read_csv(config.TRAINING_FILE)
 
-    # Get all features from train data
+    # Get all features from the dataframe
     features = [feat for feat in train_data.columns if feat not in ['id', 'target', 'kfold']]
 
-    # Fill NaN values with "NONE"
+    # Fill NaN with "NONE" after converting into string.
+    # All columns here are categorical so we can convert values to string
     for col in features:
         train_data.loc[:, col] = train_data[col].astype(str).fillna("NONE")
-
-    # Split into train and val_data
+    
+    # Split the training data into training and validation dataframes
     df_train = train_data.loc[train_data['kfold'] != fold, :].reset_index(drop=True)
     df_val = train_data.loc[train_data['kfold'] == fold, :].reset_index(drop=True)
 
-    # Concat train and val data to prepare for OHE
-    data = pd.concat([df_train[features], df_val[features]], axis=0)
+    # Merge these dataframes to fit OneHotEncoder for all the categories in training and validation dataframes
+    data = pd.concat([df_train, df_val], axis=0)
 
-    # Create object for OneHotEncoder
+    # Create object of OneHotEncoder
     ohe = OneHotEncoder()
 
-    # fit the data
+    # Fit OneHotEncoder for features in the data
     ohe.fit(data[features])
-    
-    # Transform train features 
-    x_train = ohe.transform(df_train[features])
 
-    # Transform val features
+    # Transform the features in train and validation dataframes
+    x_train = ohe.transform(df_train[features])
     x_val = ohe.transform(df_val[features])
 
-    # Create LogisticRegression model object
+    # Create object of LogisticRegression
     logres = LogisticRegression()
 
-    # Fit the train data
+    # Fit the logistic regression model over the training data
     logres.fit(x_train, df_train['target'].values)
 
-    # Predict Probabilities for each target value
-    val_proba = logres.predict_proba(x_val)[:, 1]
+    # Predict the probabilities for validation data and store predictions only for label '1'
+    y_val_preds = logres.predict_proba(x_val)[:, 1]
 
-    # Calculate the AUC score
-    print(f"AUC score: {roc_auc_score(df_val['target'].values, val_proba)}")
+    # Measure the AUC score for trained model
+    auc_score = roc_auc_score(df_val['target'].values, y_val_preds)
+
+    # Print the AUC score
+    print(f'Fold={fold}, AUC Score={auc_score}')
 
     # Save the model
     joblib.dump(logres, os.path.join(config.MODEL_OUTPUT_DIR, f'ohe_logres_fold_{fold}.bin'))
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
+    ap.add_argument('--fold', type=int, required=True, help='Fold value')
+    args = ap.parse_args()
 
-    ap.add_argument('--fold', type=int, required=True, help="Fold value")
-    params = ap.parse_args()
-
-    run(params.fold)
+    run(fold=args.fold)
