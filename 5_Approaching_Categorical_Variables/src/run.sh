42f76c003045efdb3098ab5e64f3e2498f60a72f
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-09-21 23:17:06 +0530

    Add shell script to run ohe_logres for all folds

---
+++ 5_Approaching_Categorical_Variables/src/run.sh
@@ -0,0 +1,6 @@
+# Shell script to run OHE LogisticRegression for all folds
+python3 -W ignore ohe_logres.py --fold 0
+python3 -W ignore ohe_logres.py --fold 1
+python3 -W ignore ohe_logres.py --fold 2
+python3 -W ignore ohe_logres.py --fold 3
+python3 -W ignore ohe_logres.py --fold 4
