f425525d7f17c74218bfd31cfaf91d842162a432
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-09-22 08:56:10 +0530

    Update shell script to run for any python file

--- 5_Approaching_Categorical_Variables/src/run.sh
+++ 5_Approaching_Categorical_Variables/src/run.sh
@@ -1,6 +1,18 @@
-# Shell script to run OHE LogisticRegression for all folds
-python3 -W ignore ohe_logres.py --fold 0
-python3 -W ignore ohe_logres.py --fold 1
-python3 -W ignore ohe_logres.py --fold 2
-python3 -W ignore ohe_logres.py --fold 3
-python3 -W ignore ohe_logres.py --fold 4
+#!/bin/bash
+
+# Example: $ bash run.sh ohe_logres.py
+# Change python file name after run.sh to run for other python files.
+
+echo "Training for" $1
+PY_FILE=$1
+if [ "$PY_FILE" == "" ]
+then
+    echo "Enter python file name to run. Ex: ohe_logres.py"
+else
+    # Shell script to run {python file} for all folds
+    python3 -W ignore $PY_FILE --fold 0
+    python3 -W ignore $PY_FILE --fold 1
+    python3 -W ignore $PY_FILE --fold 2
+    python3 -W ignore $PY_FILE --fold 3
+    python3 -W ignore $PY_FILE --fold 4
+fi
