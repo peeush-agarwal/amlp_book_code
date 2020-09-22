8d5429a680d2d8d516bc6e7c4ea85153d4b969b0
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-09-22 09:28:34 +0530

    Update run.sh to be able to run for ohe_LogRes, lbl_rf

--- 5_Approaching_Categorical_Variables/src/run.sh
+++ 5_Approaching_Categorical_Variables/src/run.sh
@@ -3,16 +3,23 @@
 # Example: $ bash run.sh ohe_logres.py
 # Change python file name after run.sh to run for other python files.
 
-echo "Training for" $1
 PY_FILE=$1
-if [ "$PY_FILE" == "" ]
+MODEL=$2
+if [[ $PY_FILE == "" || ($PY_FILE == "lbl_rf.py" && $MODEL == "") ]]
 then
-    echo "Enter python file name to run. Ex: ohe_logres.py"
+    echo "Enter python file name and Model name to run. Ex: 'ohe_logres.py' or 'lbl_rf.py xgb'"
 else
+    echo "Training for" $1
+    PARAM_2=""
+    if [ $PY_FILE == "lbl_rf.py" ]
+    then
+        PARAM_2="--model "$MODEL
+        echo $PARAM_2
+    fi
     # Shell script to run {python file} for all folds
-    python3 -W ignore $PY_FILE --fold 0
-    python3 -W ignore $PY_FILE --fold 1
-    python3 -W ignore $PY_FILE --fold 2
-    python3 -W ignore $PY_FILE --fold 3
-    python3 -W ignore $PY_FILE --fold 4
+    python3 -W ignore $PY_FILE --fold 0 $PARAM_2
+    python3 -W ignore $PY_FILE --fold 1 $PARAM_2
+    python3 -W ignore $PY_FILE --fold 2 $PARAM_2
+    python3 -W ignore $PY_FILE --fold 3 $PARAM_2
+    python3 -W ignore $PY_FILE --fold 4 $PARAM_2
 fi
