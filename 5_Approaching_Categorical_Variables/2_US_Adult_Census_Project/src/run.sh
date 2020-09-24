09f6cf366f5f1cf6d4dabd3386890151f2819db3
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-09-24 23:12:16 +0530

    Update shell script for multiple inputs

--- 5_Approaching_Categorical_Variables/2_US_Adult_Census_Project/src/run.sh
+++ 5_Approaching_Categorical_Variables/2_US_Adult_Census_Project/src/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-python3 -W ignore ohe_logres.py --fold 0
-python3 -W ignore ohe_logres.py --fold 1
-python3 -W ignore ohe_logres.py --fold 2
-python3 -W ignore ohe_logres.py --fold 3
-python3 -W ignore ohe_logres.py --fold 4
+echo "Training with" $1
+
+python3 -W ignore $1 --fold 0
+python3 -W ignore $1 --fold 1
+python3 -W ignore $1 --fold 2
+python3 -W ignore $1 --fold 3
+python3 -W ignore $1 --fold 4
