68ca4467c509ec1d4cd91943c26d2bdd5d9cf415
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-09-24 22:10:05 +0530

    Add config file for params

---
+++ 5_Approaching_Categorical_Variables/2_US_Adult_Census_Project/src/config.py
@@ -0,0 +1,6 @@
+COLUMNS = ['age','workclass','fnlwgt','education','education_num',
+           'marital_status','occupation','relationship','race','gender',
+           'capital_gain','capital_loss','hours_per_week','native_country',
+           'income']
+TRAINING_FILE = '../input/adult-training.csv'
+TRAINING_FOLDS_FILE = '../input/adult-training-folds.csv'
