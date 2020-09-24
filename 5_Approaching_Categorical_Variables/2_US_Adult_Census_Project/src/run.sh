45ebe770b1ccde735cc35e678cc53de866128e0b
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-09-24 22:40:29 +0530

    Add shell script to run for all folds

---
+++ 5_Approaching_Categorical_Variables/2_US_Adult_Census_Project/src/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python3 -W ignore ohe_logres.py --fold 0
+python3 -W ignore ohe_logres.py --fold 1
+python3 -W ignore ohe_logres.py --fold 2
+python3 -W ignore ohe_logres.py --fold 3
+python3 -W ignore ohe_logres.py --fold 4
